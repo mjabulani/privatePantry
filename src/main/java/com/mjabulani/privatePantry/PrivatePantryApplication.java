6e8367273ddcbc9cdf9076985e76233d372a995b
mjabulani <marekzmienka@gmail.com>
2024-02-25 23:05:37 +0100

    initial commit

---
+++ src/main/java/com/mjabulani/privatePantry/PrivatePantryApplication.java
@@ -0,0 +1,13 @@
+package com.mjabulani.privatePantry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatePantryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivatePantryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjabulani/privatePantry/PrivatePantryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjabulani.privatePantry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivatePantryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
